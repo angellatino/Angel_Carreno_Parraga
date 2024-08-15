dd0d757a8b6e3156f0867e77c016a5e693f7d87a
angellatino <angel.steven.carreno.parraga@student.ehb.be>
2024-08-15 19:41:58 +0200

    Final Repository Initialised
    Hier komt de laatste en correcte versie van mijn taak.

---
+++ src/main/java/be/ehb/angel_carreno_parraga/AngelCarrenoParragaApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.angel_carreno_parraga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngelCarrenoParragaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngelCarrenoParragaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/angel_carreno_parraga/AngelCarrenoParragaApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.angel_carreno_parraga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngelCarrenoParragaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
